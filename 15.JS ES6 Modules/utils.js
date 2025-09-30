87d10e95795f155b0c9c97c3c8778a98f026a059
Mohanapriya-Vadivel <144239583+Mohanapriya-Vadivel@users.noreply.github.com>
2025-09-30 14:30:06 +0530

    Create utils.js

---
+++ 15.JS ES6 Modules/utils.js
@@ -0,0 +1,10 @@
+// ======= utils.js =======
+
+// Function to add two numbers
+export const add = (a, b) => a + b;
+
+// Function to multiply two numbers
+export const multiply = (a, b) => a * b;
+
+// Export a constant value
+export const pi = 3.14159;
